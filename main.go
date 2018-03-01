09134fa7f86e2d912a9e1b9e82c84b189b2cb756
bis <bis83gb@gmail.com>
2018-03-01 23:50:58 +0900

    delete dirt.

--- main.go
+++ main.go
@@ -1,14 +1,8 @@
 package main
 
-import "fmt"
 import "net/http"
 
-func hHelloWorld(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprint(w, "hello, world")
-}
-
 func main() {
-  http.HandleFunc("/", hHelloWorld)
   http.Handle("/game/", http.StripPrefix("/game/", http.FileServer(http.Dir("/game"))))
-  http.ListenAndServe(":5000", nil)
+  http.ListenAndServe(":80", nil)
 }
